c731afd6536e30229ad903ca805e0670340dbaea
Devops <you@example.com>
2018-09-24 11:51:01 +0530

    initial commit

---
+++ def.java
@@ -0,0 +1,1 @@
+Worldwide, the gems and jewellery industry has been growing at a good pace and is currently estimated at over US$130 billion. In India, it accounts for nearly 20 percent of total Indian exports. 
